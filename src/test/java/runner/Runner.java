c8f30a1aba13df566447d36810915bcf93fd5de8
Ozlemcan1237 <117519510+Ozlemcan1237@users.noreply.github.com>
2023-07-30 18:18:45 +0300

    Update Runner.java

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
 
         features="./src/test/resources/features/DB_Features",
         glue= {"stepdefinitions", "hooks"},
-        tags="@login"  ,
+        tags="@db"  ,
 
         dryRun= false
 )
